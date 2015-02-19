ef488b541343a662e52a6a08398958307889b5a6
284km <at284km@gmail.com>
2015-02-19 16:20:11 +0900

    add development.rb (unused)

---
+++ config/deploy/development.rb
@@ -0,0 +1,25 @@
+
+# e.g.
+# bundle exec cap dev ls
+# bundle exec cap dev utils:perl_version
+
+# role :app, %w{vagrant@default}
+# role :web, %w{vagrant@default}
+# role :db,  %w{vagrant@default}
+
+# server 'default', user: 'vagrant', roles: %w{web app}, my_property: :my_value
+
+# namespace :utils do
+#   task :ls do
+#     on roles(:app) do
+#       execute "ls"
+#     end
+#   end
+#   task :perl_version do
+#     on roles(:app) do
+#       execute %[perl -v]
+#     end
+#   end
+# end
+
+
