bb1a3c82e55b66d9a93e0e37e6dc09ad2eab25df
284km <at284km@gmail.com>
2015-03-31 15:32:49 +0900

    modified:   db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless Rails.env.production?
+  connection = ActiveRecord::Base.connection
+  # tables = %w(table1 table2 table3)
+  # tables.each do |table|
+  #   connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
+  # end
+
+  Dir.glob("db/sql/*.sql").each do |sql_file|
+    sql = File.read(sql_file)
+    statements = sql.split(/;$/)
+    statements.pop  # the last empty statement
+    ActiveRecord::Base.transaction do
+      statements.each do |statement|
+        connection.execute(statement)
+      end
+    end
+  end
+end
